0107b100381301e1d737c9c33f1beec08ec7682b
StephenOosthuizen <StephenDiavolo@gmail.com>
2024-02-13 16:57:01 +0200

    Add project files.

---
+++ StephenTechInterviewPack/Custom Data Structures/StephenSinglyLinkedList.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StephenTechInterviewPack.Custom_Data_Structures
+{
+    internal class StephenSinglyLinkedList
+    {
+    }
+}
