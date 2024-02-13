c4a98734add56b3de0058c56cfc10cc751c8e21f
StephenOosthuizen <StephenDiavolo@gmail.com>
2024-02-13 17:18:35 +0200

    Added some basic custom Singly Linked List logic

--- StephenTechInterviewPack/Custom Data Structures/StephenSinglyLinkedList.cs
+++ StephenTechInterviewPack/Custom Data Structures/StephenSinglyLinkedList.cs
@@ -8,5 +8,45 @@ namespace StephenTechInterviewPack.Custom_Data_Structures
 {
     internal class StephenSinglyLinkedList
     {
+        public Node Root;
+
+        //doing the method like this allows us to call GetRoot, without using brackets "()" -> just looks a bit nicer
+        public Node GetRoot { get { return Root; } }
+
+        private Node GetLastNode
+        {
+            get
+            {
+                if (Root == null)
+                    return null;
+
+                //doing an Else statement/check here takes up a clock-cycle, which adds latency.
+                //We know that the program will only ever reach this point if the Root node is not empty
+
+                Node node = Root;
+                while (node.Next != null)
+                {
+                    //move the node to the next node in the list
+                    node = node.Next;
+                }
+                return node;
+            }
+        }
+
+        public void AddNode(object data)
+        {
+            Node n = new Node { Data = data };
+
+            if (Root == null)
+                Root = n;
+            else
+                GetLastNode.Next = n;
+        }
+
+        public class Node
+        {
+            public Node Next;
+            public object Data;
+        }
     }
 }
